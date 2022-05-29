cb0b2ea8f8454dbd94c74a97a1f9823db4c10df4
shigerukenzo <shigerukenzo@gmail.com>
2022-05-29 09:07:03 +0200

    Finished create table queries

--- CreateTables.py
+++ CreateTables.py
@@ -16,8 +16,33 @@ def get_create_tables_query():
                                     FOREIGN KEY (MemberID),
                                     PRIMARY KEY (ExerciseID, SetID, MemberID)
                                 );"""
+
     set_table = """ CREATE TABLE IF NOT EXISTS set (
                                 set integer PRIMARY KEY,
                                 reps integer NOT NULL,
                                 success integer NOT NULL
                             ); """
+
+    exercise_table = """ CREATE TABLE IF NOT EXISTS Exercise (
+                                exerciseID integer PRIMARY KEY,
+                                FOREIGN KEY (muscleGroupID),
+                                exerciseName text NOT NULL
+                            ); """
+
+    exerciseMuscleGroup_table = """ CREATE TABLE IF NOT EXISTS ExerciseMuscleGroup (
+                                FOREIGN KEY (ExerciseID),
+                                FOREIGN KEY (muscleGroupID)
+                            ); """
+
+    muscleGroup_table = """ CREATE TABLE IF NOT EXISTS MuscleGroup (
+                                muscleGroupID integer PRIMARY KEY,
+                                muscleGroupName text NOT NULL
+                            ); """
+
+    muscle_table = """ CREATE TABLE IF NOT EXISTS Muscle (
+                                muscleID integer PRIMARY KEY,
+                                FOREIGN KEY (muscleGroupID),
+                                muscleName text NOT NULL
+                            ); """
+
+    return member_table, memberExcercise_table, exercise_table, exerciseMuscleGroup_table, muscleGroup_table, muscle_table, set_table
