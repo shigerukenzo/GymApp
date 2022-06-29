4d4858ee56c86f7d4fa94fa7549f1df969d92950
shigerukenzo <shigerukenzo@gmail.com>
2022-06-29 10:54:00 +0200

    Added missing attributes in database. Added member signup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sqlite3
 from sqlite3 import Error
 from CreateTables import get_create_tables_query
 import connection
+from datetime import datetime
 from dashboard import Member
 
 
@@ -27,7 +28,23 @@ def login(db):
         member.main()
     return
 
-def signup():
+def signup(db):
+    email = input("Enter Email")
+    query = f"SELECT * FROM member WHERE email = {email}" # Query to check if email exists
+    if not db.member_validation(query): # Handle invalid email
+        print("Email is not available")
+        return
+
+    password = hash(input("Enter password\n"))
+    first_name = input("Enter first name\n")
+    last_name = input("Enter last name\n")
+    start_date = datetime.today().strftime('%Y-%m-%d')
+
+    query = f"INSERT INTO member (MemberID,firstName, lastName,startDate,email, password)" \
+            f"VALUES ({hash(email)}, {first_name}, {last_name}, {start_date}, {email}, {password})"
+
+    
+
     return
 
 def main():
